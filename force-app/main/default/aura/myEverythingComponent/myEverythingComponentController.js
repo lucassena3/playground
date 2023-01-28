12658042b696250ba80261aac91cc05079f73555
Lucas Sena <lucas.souza@everymind.com.br>
2023-01-28 17:56:12 -0300

    Update classes playground trailhead

--- force-app/main/default/aura/myEverythingComponent/myEverythingComponentController.js
+++ force-app/main/default/aura/myEverythingComponent/myEverythingComponentController.js
@@ -1,7 +1,5 @@
 ({
     myAction : function(component, event, helper) {
-        var counter = component.get("v.myAttribute");
-        counter++;
-        component.set("v.myAttribute", counter);
-    },
+
+    }
 })
